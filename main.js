97f5b7d501aea31cf0a8343da3e9b2c628553f8b
Tech-A20 <Tech-A20@Tech-A20s-MacBook-Air.local>
2017-10-31 11:47:29 +0000

    naming and annoymous functions with parameters

---
+++ main.js
@@ -0,0 +1,57 @@
+// var result = 5 + 10;
+// var result2 = 10 + 10;
+// var result4 = 11 -2;
+//
+// console
+
+//this is a named function
+// function addNumbers() {
+//   var result = 5 + 10;
+//   console.log(result);
+// }
+
+//I'm calling the function here
+// addNumbers();
+
+//This is an anonymous function
+// var anonAddNumbers = function() {
+//   var result = 5 + 10;
+//   console.log(result);
+// }
+//
+// anonAddNumbers();
+
+//
+// function msg(){
+//   msg ='Hello world'
+//   console.log(msg);
+// }
+// msg();
+//
+// var msgs = function() {
+//   var Hi = 'Hello world'
+//   console.log(Hi);
+//   }
+//   msgs();
+// makeNoise();
+//   function makeNoise(){
+//     alert("how annoying");
+//   }
+
+
+  // function say(){
+  //   word = "this is my world"
+  //   console.log(word);
+  // }
+  // say();
+
+
+
+
+
+// function addNumbers(number1, number2) {
+//     return number1 + number2;
+//   }
+// var answer1 = addNumbers(1,3);
+// debugger;
+// console.log(answer1);
